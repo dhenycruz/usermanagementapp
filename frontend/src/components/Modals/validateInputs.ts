b985db4d4a8615b7290ea8333d2b0840a9784895
Dheniarley Cruz <dheniarley.ds@gamil.com>
2022-06-19 22:55:28 -0300

    Fix bug charactere input name.

--- frontend/src/components/Modals/validateInputs.ts
+++ frontend/src/components/Modals/validateInputs.ts
@@ -23,8 +23,8 @@ export const validateNumberName = (name:string, messagesArray: string[]): string
 export const validateCharacterName = (name: string, messagesArray: string[]): string[] => {
   // Validate if there is a special character in the name
   // reference: https://pt.stackoverflow.com/questions/342605/verificar-a-exist%C3%AAncia-de-caracteres-especiais-em-uma-string-utilizando-regexp
-  const verifyCharacter = /\W|_/;
-  if (verifyCharacter.test(name.replace(/ /g, ""))) {
+  const verifyCharacter = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/;
+  if (!verifyCharacter.test(name.replace(/ /g, ""))) {
     const filterExistsError = messagesArray.some((message) => message === 'Nome não pode haver caracteres especiais.');
     if (!filterExistsError) {
       messagesArray.push('Nome não pode haver caracteres especiais.');
